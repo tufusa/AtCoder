e0bd430aeb21be3b910c83e8a3d4349a521dce47
tufusa <tufusa74@gmail.com>
2024-02-08 09:16:47 +0900

    abc187/c

--- abc/abc187/c.rb
+++ abc/abc187/c.rb
@@ -2,9 +2,9 @@ n = gets.to_i
 s = n.times.map{ gets.chomp }.uniq
 a, b = s.partition{ |i| i[0] == "!"} #先頭が"!"であるものとそうでないものに分ける
 a.map{ |i| i.delete!("!")}
-f = a & b #どちらにも属する要素が"!"を付けても付けなくても存在する文字列
-if f == []
+f1 = a & b #どちらにも属する要素が"!"を付けても付けなくても存在する文字列
+if f1 == []
     puts "satisfiable"
 else
-    puts f[0]
+    puts f1[0]
 end
