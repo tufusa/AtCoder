a191e67daae1bca970422f7bd7b10596b4079a29
tufusa <tufusa74@gmail.com>
2024-02-08 09:17:18 +0900

    abc203/c

--- abc/abc203/c.rb
+++ abc/abc203/c.rb
@@ -1,10 +1,10 @@
 n, k = gets.split.map(&:to_i)
 fri = n.times.map{ gets.split.map(&:to_i) }.sort_by(&:first)
-fri.each do |f|
-    if k < f[0]
+fri.each do |f1|
+    if k < f1[0]
         break
     else
-        k += f[1]
+        k += f1[1]
     end
 end
 puts k
